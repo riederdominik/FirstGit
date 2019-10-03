6a64d31999404d29ee1286f8eb3752da5d9a6289
riederdominik <rieder.dominik@gmx.at>
2019-10-03 10:08:48 +0200

    Added HelloWorld Scene and updated Build Settings

---
+++ Assets/Scripts/FirstDebug.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FirstDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First git");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
